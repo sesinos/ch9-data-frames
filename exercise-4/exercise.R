4af3fcab485cb1297c7e4182bd3939f9f7cd9361
Sesinos Zerbabiel <Sesinos@D-69-91-179-63.dhcp4.washington.edu>
2018-04-11 16:31:17 -0700

    complete

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -4,14 +4,15 @@
 # file into a variable called `grants` using the `read.csv()`
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
-
+grants <- read.csv('data/gates_money.csv', stringsAsFactors=FALSE)
 
 # Use the View function to look at the loaded data
-
+View(grants)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
 
+organization 
 
 # Confirm that the "organization" column is a vector using the `is.vector()` 
 # function. 
